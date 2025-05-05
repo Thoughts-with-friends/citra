5980ac6032cde51e37a2d069e17c6bdaaba146ec
DiaSird <78157340+DiaSird@users.noreply.github.com>
2025-05-05 18:21:44 +0900

    build: add PowerShell script for MSVC local build setup

---
+++ build.ps1
@@ -0,0 +1,37 @@
+# MSVC Local Build: ./build.ps1 -init
+Param(
+    # [switch]
+    # $f, 
+    [switch]
+    $init
+)
+
+$env:CCACHE_DIR = "./.ccache"
+$env:CCACHE_COMPILERCHECK = "content"
+$env:CCACHE_SLOPPINESS = "time_macros"
+
+if ($init) {
+    git config --global core.autocrlf input
+}
+
+# deps:
+# ccache ninja wget ctest python3(used by aqt command)
+cmake -G Ninja `
+    -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl -B build `
+    -DCMAKE_LINKER=link `
+    -DCMAKE_BUILD_TYPE=Release `
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache `
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache `
+    -DENABLE_QT_TRANSLATION=ON `
+    -DCITRA_ENABLE_COMPATIBILITY_REPORTING=ON `
+    -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON `
+    -DUSE_DISCORD_PRESENCE=ON `
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+ninja
+ninja bundle
+
+ccache -s -v
+ctest -VV -C Release || Write-Output "::error ::Test error occurred on Windows build"
+
+# bash.exe ./.ci/pack.sh
+# cmake --build ./build -j $Env:NUMBER_OF_PROCESSORS
